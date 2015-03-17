48b3bc7c87f72ff114dca0067594b958dbde8088
oacastillol <osmar.alejandro.castillo@gmail.com>
2015-03-16 21:04:18 -0500

    V 0.0000001 esqueleto servidor

--- Practica2/p2-dogServer.c
+++ Practica2/p2-dogServer.c
@@ -1,7 +1,24 @@
-# include<stdio.h>
+# include <stdio.h>
+# include <netienet/in.h> // esta libreria contiene la estructura sockaddr_in
+# include <sys/socket.h> // contiene a socklen_t
+
+
+
+#define PORT 3141
 
 int main(){
 
-
+	int serverId,
+	struct sockaddr_in server, client;
+	socklen_t tamano;
+	
+	serverId=socket(AF_INET,SOCK_STREAM,0);
+	if(serverId<0){
+		perror("\nError en socket()\n");
+		exit(-1);
+	}
+	
+	
+	
 
 }
